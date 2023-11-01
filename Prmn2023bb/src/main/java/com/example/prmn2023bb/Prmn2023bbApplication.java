708971f571a180373ee892af79bcf345aa270375
[NCcreate] <[b2220920@photon.chitose.ac.jp]>
2023-11-01 09:32:17 +0900

    プロジェクト作成

---
+++ Prmn2023bb/src/main/java/com/example/prmn2023bb/Prmn2023bbApplication.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023bb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2023bbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2023bbApplication.class, args);
+    }
+
+}
